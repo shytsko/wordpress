9d9da2b527f1cb4fa239455a0a4b7865af1a7c91
Vladimir Shytsko <shytsko.v@gmail.com>
2022-12-06 08:07:00 +0000

    Init

---
+++ ssl_renew.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+DOCKER="/usr/bin/docker"
+
+cd /home/webmaster/wordpress/
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$DOCKER system prune -af
+
